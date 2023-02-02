d64e2fc718b38516a3b79baf893e80582577ce2c
PedroBSanchez <pedro_b47@hotmail.com>
2023-02-01 21:51:31 -0300

    Fix: fuso horário]

--- src/database/schemas/OrderSchema.ts
+++ src/database/schemas/OrderSchema.ts
@@ -4,15 +4,16 @@ import { Order } from "../../models/Order";
 
 const moment = require("moment-timezone");
 
-const dateBrasil = moment.tz(Date.now(), "America/Sao_Paulo");
-
 const OrderSchema = new Schema<Order>(
   {
     city: { type: String, required: true },
     client: { type: String, required: true },
     items: { type: [Object], required: true },
     total: { type: Number, required: true },
-    createdAt: { type: Date, default: dateBrasil },
+    createdAt: {
+      type: Date,
+      default: moment.tz(Date.now(), "America/Sao_Paulo"),
+    },
     seller: { type: Object, required: true },
     code: { type: Number, default: 0 },
   },
